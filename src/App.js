86c7ac08fb3bcbb29e495312d6cc6cde6d5b00b6
Jerry <jerry.yuan@webweye.com>
2022-03-03 15:23:02 +0800

    change text again

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Happy Hacking</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Let's have fun with github actions!</h3>
       </header>
     </div>
   );
